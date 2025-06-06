2dc1e5e7e6a2aac39b7ddcf1dee7c36b31316c24
Pedro <pedrogabrielbhz@gmail.com>
2025-06-06 09:55:51 -0300

    init

---
+++ src/main/java/com/comp/demo/rest/RestController.java
@@ -0,0 +1,11 @@
+package com.comp.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hi.";
+    }
+}
